bb4a3fd929628e7c0d166f3b00fe50630f807993
feifan zhou <superzhou7@gmail.com>
2021-02-17 15:59:42 +0800

    添加缓存控制

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const http = require("http");
+const crypto = require('crypto');
 const Path = require("path");
 const fs = require("fs");
 
 // todo 添加gzip压缩
 // todo 缓存控制
 const server = http.createServer(function (req,res){
-    const fileName=Path.resolve(__dirname,"."+req.url);
-    const extName=Path.extname(fileName).substr(1);
-
+    const fileName = Path.resolve(__dirname,"."+req.url);
+    const extName = Path.extname(fileName).substr(1);
+    // res.setHeader('Cache-Control', 'max-age=20')
+    const buffer = fs.readFileSync(fileName);
+    const hash = crypto.createHash('md5').update(buffer, 'utf8').digest('hex');
+    res.setHeader('Cache-Control', 'max-age=300')
+    res.setHeader('Etag', hash)
+    // res.setHeader('last-modified', new Date().toUTCString())
+    // if (new Date(req.headers['if-modified-since']).getTime() + 30 * 1000 > Date.now()) {
+    //     console.log('协商缓存命中....')
+    //     res.statusCode = 304
+    //     res.end()
+    //     return
+    // }
+    if(req.headers['if-none-match'] === hash){
+        console.log('Etag协商缓存命中.....')
+        res.statusCode = 304
+        res.end()
+        return 
+      }
     if (fs.existsSync(fileName)) { //判断本地文件是否存在
         var mineTypeMap={
             html:'text/html;charset=utf-8',
